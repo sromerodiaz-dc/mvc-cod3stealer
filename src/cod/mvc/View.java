33d8f0ab49948bccefe5f3d89760378fa3dc0ebd
cod3stealer <sromerodiaz@danielcastelao.org>
2024-05-31 09:28:35 +0200

    Código cambiado

--- src/cod/mvc/View.java
+++ src/cod/mvc/View.java
@@ -1,8 +1,16 @@
 package cod.mvc;
 
+/**
+ * Clase que representa la vista de la aplicación, responsable de mostrar la información al usuario.
+ */
 public class View {
-    public static boolean muestraVelocidad(String matricula, Integer v){
-        System.out.println("[View] " + matricula + ": " + v + "km/hr");
-        return true;
+    /**
+     * Muestra la velocidad del coche en la consola.
+     *
+     * @param matricula Matrícula del coche
+     * @param v Velocidad del coche en km/hr
+     */
+    public static void muestraVelocidad(String matricula, Integer v){
+        System.out.println("Radar => " + matricula + ": " + v + " km/hr");
     }
 }
