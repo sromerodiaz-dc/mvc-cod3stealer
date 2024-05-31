fe13a92d9c8790a686e05a1b54970bda69bb3cbd
cod3stealer <sromerodiaz@danielcastelao.org>
2024-05-31 09:26:32 +0200

    más JavaDoc modificado y completado

--- src/cod/mvc/ObserverLimite.java
+++ src/cod/mvc/ObserverLimite.java
@@ -1,22 +1,25 @@
 package cod.mvc;
 
 /**
- * Otro observador que se suscribe a los cambios de velocidad de un coche
+ * Observador de límite de velocidad.
  */
 public class ObserverLimite implements Observer {
-    // velocidad máxima
+    // Velocidad máxima permitida
     final static int LIMITE = 120;
+
     /**
-     * Este es el observador de limite de velocidad
+     * Método que se ejecutará cuando el objeto observado cambie.
+     * En este caso, se verifica si el coche ha excedido el límite de velocidad.
+     * Si el coche ha excedido el límite, se mostrará un mensaje de "INFRACCION"
+     * y se reducirá la velocidad del coche en 10 km/h.
      * @param coche Coche al que se le actualizó la velocidad
      */
     @Override
     public void update(Coche coche, Model model) {
-        if(coche.velocidad>LIMITE){
-            System.out.println("INFRACCION"+
+        if (coche.velocidad > LIMITE) {
+            System.out.println("INFRACCION" +
                     "\nSe ha reducido la velocidad");
-            model.cambiarVelocidad(coche.matricula, coche.velocidad-10);
-
+            model.cambiarVelocidad(coche.matricula, coche.velocidad - 10);
         }
     }
 }
