4efc0008a56269646993b8dffc310f3351f71bd0
cod3stealer <sromerodiaz@danielcastelao.org>
2024-05-31 09:25:57 +0200

    JavaDoc modificado y completado

--- src/cod/mvc/Observable.java
+++ src/cod/mvc/Observable.java
@@ -1,13 +1,25 @@
 package cod.mvc;
 
 /**
- * Interfaz Observable
+ * Interfaz Observable, define los métodos para agregar, eliminar y notificar observadores.
  */
 public interface Observable {
-    // añade un observador
+    /**
+     * Añade un observador a la lista de observadores.
+     * @param observer el observador a agregar
+     */
     void addObserver(Observer observer);
-    // elimina un observador
+
+    /**
+     * Elimina un observador de la lista de observadores.
+     * @param observer el observador a eliminar
+     */
     void removeObserver(Observer observer);
-    // notifica a los observadores
-    void notifyObservers(Coche coche,Model model);
+
+    /**
+     * Notifica a todos los observadores registrados cuando ocurre un cambio en el estado del objeto observable.
+     * @param coche el coche que ha cambiado
+     * @param model el modelo actual
+     */
+    void notifyObservers(Coche coche, Model model);
 }
