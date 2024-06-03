e7309849964e928dc3d255f3204cd79501f63ef7
cod3stealer <sromerodiaz@danielcastelao.org>
2024-06-03 08:52:45 +0200

    Implementación de nuevo límite mínimo para poder usar ambos métodos de manera efectiva!

--- src/cod/mvc/ObserverLimite.java
+++ src/cod/mvc/ObserverLimite.java
@@ -5,18 +5,23 @@ package cod.mvc;
  */
 public class ObserverLimite implements Observer {
     // velocidad máxima
-    final static int LIMITE = 120;
+    final static int LIMITE_MAX = 120;
+    final static int LIMITE_MIN = 60; // Añado un mínimo para implementar el método bajarVelocidad
+
     /**
      * Este es el observador de limite de velocidad
      * @param coche Coche al que se le actualizó la velocidad
      */
     @Override
     public void update(Coche coche, Model model) {
-        if(coche.velocidad>LIMITE){
+        if(coche.velocidad>LIMITE_MAX){
             System.out.println("INFRACCION"+
                     "\nSe ha reducido la velocidad");
-            model.cambiarVelocidad(coche.matricula, coche.velocidad-10);
-
+            model.bajarVelocidad(coche.matricula, 10);
+        } else if (coche.velocidad<LIMITE_MIN){
+            System.out.println("INFRACCION"+
+                    "\nSe ha aumentado la velocidad");
+            model.subirVelocidad(coche.matricula, 10);
         }
     }
 }
